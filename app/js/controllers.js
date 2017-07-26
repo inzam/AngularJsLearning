099b789594736f070f43f1f7dd9922273cf66213
Inzam Zaman <inzam.zaman@inzam.com>
2017-07-27 03:29:05 +0600

    AngularJSLearning
    Learning AgularJS by ninjaToNovice

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,6 +7,15 @@ function MyCtrl1() {}
 MyCtrl1.$inject = [];
 
 
-function MyCtrl2() {
-}
+function MyCtrl2() {}
 MyCtrl2.$inject = [];
+
+//FinanceMeter
+angular.module('myApp.controllers', []).
+controller('FinanceController', function ($scope) {
+    $scope.salary = 0;
+    $scope.percentage = 0;
+    $scope.result = function () {
+        return $scope.salary * $scope.percentage * 0.01;
+    };
+});
